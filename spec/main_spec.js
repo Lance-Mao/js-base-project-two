7ac6beb4b5a640817a19431f90d57692325b346e
jtong <tj19832@gmail.com>
2015-12-02 16:29:34 +0800

    init

---
+++ spec/main_spec.js
@@ -0,0 +1,31 @@
+"use strict";
+var _ = require("lodash");
+var chai = require("chai");
+var sinon = require("sinon");
+var sinonChai = require("sinon-chai");
+var expect = chai.expect;
+chai.use(sinonChai);
+
+var main = require("../lib/main.js");
+
+
+describe("测试描述", function(){
+    sinon.spy(console, 'log');
+
+    it("测试用例1", function(){
+
+        var result = main();
+        var expect_string = '';
+        
+        expect(expect_string).to.equal(result);
+    });
+
+    it("测试用例2", function(){
+
+        main();
+        var result = _.flatten(console.log.args).join("\n");
+        var expect_string = '';
+
+        expect(expect_string).to.equal(result);
+    });
+});
